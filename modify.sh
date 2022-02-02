a726a1f294d49345d60ab1c5457ccc59181fb636
Zachary Crimmel <70920329+ZuhChar@users.noreply.github.com>
2022-02-02 16:50:31 -0700

    Add files via upload

--- modify.sh
+++ modify.sh
@@ -1,7 +1,22 @@
 #! /bin/bash
 
+#Check to see if there is no arguments passed to the script
 if [ $# -eq 0 ]; 
 then
     echo "usage: modify filename [filename...]"
     exit 1
+fi
+
+for arguments in $#;
+    if [[ $# = *.txt ]];
+    then 
+        echo "This is a text file"
+    elif [[ -d $# ]];
+    then
+        echo "modify: $#, is a directory."
+    elif [[ $# = *.exe ]];
+    then
+        echo "modify: $#, cannot edit executables."
+    else
+        echo "modify: $#, filetype not supported."
 fi
