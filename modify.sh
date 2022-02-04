4c6279df5750c4a5221df125eb8cec91e24e18b6
ZuhChar <zcrimmel@uwyo.edu>
2022-02-03 21:57:43 -0700

    ed

--- modify.sh
+++ modify.sh
@@ -17,7 +17,7 @@ TIME=$(date +"%H%M")
 for argument in $@; do
     if file "$argument" | grep ASCII;
     then 
-        ed $argument 1,1m2
+        ed 1,1m2 "$argument"
         sed -i 's/Gradez/Grades/g' "$argument"
         ed -s $argument <<END
             r !date +'Modified on: $DAY, $MONTH, $YEAR: $TIME \%nModified by: zachary_crimmel'
