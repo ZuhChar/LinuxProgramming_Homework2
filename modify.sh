1a6c99923dd7569f1930acfb29843c6552442151
ZuhChar <zcrimmel@uwyo.edu>
2022-02-03 17:56:42 -0700

    ed

--- modify.sh
+++ modify.sh
@@ -18,7 +18,7 @@ for argument in $@; do
     if [[ $argument == *.txt ]];
     then 
         ed -s $argument <<END
-            r !date +'Modified on: $DAY, $MONTH, $YEAR: $TIME %nModified by: zachary_crimmel'
+            r !date +'Modified on: $DAY, $MONTH, $YEAR: $TIME \nModified by: zachary_crimmel'
             w $argument
             q
 END
