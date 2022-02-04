154f06bb366ca4c9bb36b1226a8d5bdbc0b9d3f2
ZuhChar <zcrimmel@uwyo.edu>
2022-02-03 19:27:53 -0700

    ed

--- modify.sh
+++ modify.sh
@@ -17,11 +17,11 @@ TIME=$(date +"%H%M")
 for argument in $@; do
     if [[ $argument == *.txt ]];
     then 
-        ed -s $argument <<,
+        ed -s $argument <<END
         s/Gradez/Grades
         w $argument
         q
-,
+END
         ed -s $argument <<END
             r !date +'Modified on: $DAY, $MONTH, $YEAR: $TIME \%nModified by: zachary_crimmel'
             w $argument
