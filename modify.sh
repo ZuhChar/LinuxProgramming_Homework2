e37934d8b08435d91fbcafd316b1cf3e482ca1aa
ZuhChar <zcrimmel@uwyo.edu>
2022-02-03 18:45:30 -0700

    ed

--- modify.sh
+++ modify.sh
@@ -22,11 +22,11 @@ for argument in $@; do
             w $argument
             q
 END
-        sed -e $argument <<$
+        sed -e $argument <<%
         s/Gradez/Grades
         w $argument
         q
-$
+%
     elif [ -d "$argument" ];
     then
         echo "modify: $argument, is a directory."
